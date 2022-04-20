aea8341998979e62db4d254266e27a21d6b29fc7
SakshamGarg1 <86765712+SakshamGarg1@users.noreply.github.com>
2022-04-20 23:01:37 +0530

    Add files via upload

---
+++ block-BNaacn/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var mongoose = require('mongoose');
+
+mongoose.connect(
+    "mongodb://localhost/Schema",
+    { useNewUrkParser: true , useUnifiedTopology: true },
+    (err)=>{
+        console.log(err ? err: " connect true")
+})
+
+
+var app = express();
+
+app.get('/',(req,res)=>{
+    res.send('weolcome')
+})
+
+app.listen(3000,()=>{
+    console.log('server working on 3000')
+})
